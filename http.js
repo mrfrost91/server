b63dc7b2e90c4a509a5032df069f076b97e1f35b
mrfrost91 <metallfrost@gmail.com>
2018-10-23 01:19:17 +0300

    Update http.js
    123

--- http.js
+++ http.js
@@ -4,17 +4,17 @@ var fs = require('fs');
 console.log("razmatyvaem kishki 10 minut")
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = q.pathname;
+    var q = url.parse(req.url, true);
+    var filename = q.pathname;
     if (filename === '/items/') {
-      fs.readFile("items/id.json", function (err, data) {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.write(data)
-      return res.end();
-    })
-    }  
+        fs.readFile("items/id.json", function (err, data) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(data)
+            return res.end();
+        })
+    }
     else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    return res.end("404 Not Found");
-  }
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+    }
 }).listen(8080);
