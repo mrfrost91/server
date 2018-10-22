84a22409cf60838c1606ecada119af8c850e6d01
mrfrost91 <metallfrost@gmail.com>
2018-10-23 00:46:34 +0300

    Update http.js
    Da eto zhostka

--- http.js
+++ http.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+console.log("razmatyvaem kishki 10 minut")
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
-    console.log(q)
   var filename = q.pathname;
-  fs.readFile("items/id.json", function (err, data) {
     if (filename === '/items/') {
+      fs.readFile("items/id.json", function (err, data) {
       res.writeHead(200, {'Content-Type': 'application/json'});
       res.write(data)
       return res.end();
+    })
     }  
+    else {
     res.writeHead(404, {'Content-Type': 'text/html'});
     return res.end("404 Not Found");
-  });
+  }
 }).listen(8080);
