bbb3ea9603ce5ab77f98993bf6b4fea8c6217854
mrfrost91 <metallfrost@gmail.com>
2018-10-23 12:51:27 +0300

    123

--- http.js
+++ http.js
@@ -6,11 +6,13 @@ console.log("razmatyvaem kishki 10 minut")
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = q.pathname;
+    console.log(filename)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     if (filename === '/items/') {
         fs.readFile("items/id.json", function (err, data) {
             res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(data)
-            return res.end();
+            return res.end(data);
         })
     }
     else {
