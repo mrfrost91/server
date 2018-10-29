f28527228bc8dce92c5cbbed121fc76316995cb8
mrfrost91 <metallfrost@gmail.com>
2018-10-29 19:25:19 +0300

    123

--- http.js
+++ http.js
@@ -1,22 +1,21 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-console.log("razmatyvaem kishki 10 minut")
+var PORT_NAME = 8080;
+var CODE_SUCCESS = 200;
+var CODE_FAIL = 404;
+console.log("razmatyvaem kishki 10 minut");
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    var filename = q.pathname;
-    console.log(filename)
+    var pathname = q.pathname;
+    console.log(pathname);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    if (filename === '/items/') {
+    if (pathname === '/items/') {
         fs.readFile("items/id.json", function (err, data) {
-            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.writeHead(CODE_SUCCESS, {'Content-Type': 'application/json'});
             return res.end(data);
         })
+    } else {
     }
-    else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-    }
-}).listen(8080);
